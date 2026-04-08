14ff04dc9197ced54201471b18629dbb5f04437a
kev <jankevin.dionaldo64@gmail.com>
2026-04-08 19:44:19 +0800

    feat: added double side in the line 55 of the mirror

--- js/components/textured-model.js
+++ js/components/textured-model.js
@@ -52,6 +52,7 @@
         color: new THREE.Color("#d9d9d9"),
         roughness: 0.08,
         metalness: 0.85,
+        side: THREE.DoubleSide,
       });
     } else {
       material = new THREE.MeshStandardMaterial({
